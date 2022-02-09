6ea2a71f8c604f712d96295ecdb014defa5510b1
arjaydevs <99182989+arjaydevs@users.noreply.github.com>
2022-02-10 04:27:56 +0800

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+// new filename
+$filename = 'pic_'.date('YmdHis') . '.jpeg';
+
+$url = '';
+if( move_uploaded_file($_FILES['webcam']['tmp_name'],'upload/'.$filename) ){
+	$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/upload/' . $filename;
+}
+
+// Return image url
+echo $url;
